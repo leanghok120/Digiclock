daf64125b510748b8490c3c870bf50a7ca8b9e7d
leanghok120 <leanghokoeng5@gmail.com>
2024-08-25 08:29:34 +0700

    Finish clock component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Clock from "./Clock";
+
+function App() {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      <Clock />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Clock.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+
+function Clock() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  function formatTime() {
+    const hours = addZero(time.getHours());
+    const min = addZero(time.getMinutes());
+    const sec = addZero(time.getSeconds());
+
+    return `${hours}:${min}:${sec}`;
+  }
+
+  function addZero(num) {
+    return (num < 12 ? "0" : "") + num;
+  }
+
+  return (
+    <div className="mt-8">
+      <h2 className="text-center text-8xl font-black tracking-wider">
+        {formatTime()}
+      </h2>
+    </div>
+  );
+}
+
+export default Clock;
