f5f2dd67f02871d2f222b4c45c91528576e33c80
leanghok120 <leanghokoeng5@gmail.com>
2024-08-25 13:20:41 +0700

    Remove background switching

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
-import { useState } from "react";
 import Clock from "./Clock";
 
 function App() {
-  // index for background
-  const [index, setIndex] = useState("1");
-
-  function handleClick() {
-    const newIndex = index == "1" ? "2" : "1";
-    setIndex(newIndex);
-  }
-
   return (
     <div
-      className={`flex flex-col justify-center items-center h-screen bg-[url('/src/assets/background${index}.jpg')] bg-fixed bg-no-repeat bg-cover bg-center bg-white`}
-      onClick={handleClick}
+      className={`flex flex-col justify-center items-center h-screen bg-[url(/src/assets/background1.jpg)] bg-fixed bg-no-repeat bg-cover bg-center bg-white`}
     >
       <Clock />
     </div>
